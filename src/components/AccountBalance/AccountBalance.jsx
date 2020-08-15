762be2b9c0743431bb3e14513ef097281ef40e54
Claude Bourgeois <cbourgeois@vtx.ch>
2020-08-15 19:31:46 +0200

    styled components update

---
+++ src/components/AccountBalance/AccountBalance.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Section = styled.section`
+    font-size: 2rem;
+    text-align: left;
+    padding: 1.5rem 0 1.5rem 5rem;
+`;
+
+export default class AccountBalance extends Component {
+    render() {
+        return (
+            <>
+                <Section>
+                    Balance: ${this.props.amount}
+                </Section>
+            </>
+        )
+    }
+}
+
+AccountBalance.propTypes = {
+    amount: PropTypes.number.isRequired
+}
